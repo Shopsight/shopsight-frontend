82fa28d6cc6e4b233dd304d1df43a05b16faaf06
Shubham Mondal <shubhamank002@gmail.com>
2022-12-27 23:00:39 +0530

    Update brand card

--- src/home/components/Brands.js
+++ src/home/components/Brands.js
@@ -1,24 +1,23 @@
-import { useEffect, useState } from "react";
 import BrandItem from "./BrandItem";
 
 const Brands = () => {
-    const brandCount = 3;
-    const [topBrands, setTopBrands] = useState([]);
-
-    const fetchTopBrands = async () => {
-        try {
-            const url = `${process.env.REACT_APP_SERVER_URL}/api/brand/top/${brandCount}`;
-            const res = await fetch(url);
-            const data = await res.json();
-            setTopBrands(data.topBrands);
-        } catch (err) {
-            setTopBrands([]);
-        }
-    };
-
-    useEffect(() => {
-        fetchTopBrands();
-    }, []);
+    const topBrands = [
+        {
+            id: 1,
+            name: "Raymond",
+            logo: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        },
+        {
+            id: 2,
+            name: "Puma",
+            logo: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        },
+        {
+            id: 3,
+            name: "Nike",
+            logo: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        },
+    ];
 
     return (
         <div className="brands-container">
