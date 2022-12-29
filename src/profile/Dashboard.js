ea7c214f6adedc81f550901cbd06bb23bb41bcdb
Shubham Mondal <shubhamank002@gmail.com>
2022-12-29 19:12:38 +0530

    Update dashboard ui

--- src/profile/Dashboard.js
+++ src/profile/Dashboard.js
@@ -23,7 +23,6 @@ const Dashboard = () => {
                 },
             });
             const data = await res.json();
-            console.log(data);
             setUser(data);
         } catch (err) {
             // window.alert("Something went wrong");
@@ -38,35 +37,43 @@ const Dashboard = () => {
         <div>
             <div className="user-info-container">
                 <div className="user-info-type">
-                    <h3>Email</h3>
-                    <h2>{user.email}</h2>
-                </div>
-                <div className="user-info-type">
-                    <h3>Name</h3>
-                    <h2>{user.name}</h2>
+                    <h1>Welcome back {user.name}</h1>
                 </div>
             </div>
             <div>
-                <h1 className="products-title">Your Wishlist</h1>
-                <div className="products-container">
-                    {user.favourites &&
-                        user.favourites.map((product) => (
-                            <div className="product-card-container">
-                                <Link to={`/product/${product.id}`} className="product-card-image">
-                                    <img src={product.imageLink} alt={product.name} />
-                                </Link>
-                                <div className="product-card-info">
-                                    <div className="product-card-info-left">
-                                        <div className="product-card-brand">Raymond</div>
-                                        <div className="product-card-name">{product.name}</div>
-                                        <div className="product-card-price">
-                                            Rs. {product.price}
+                {user.favourites && user.favourites.length ? (
+                    <>
+                        <h1 className="products-title">Your Wishlist</h1>
+                        <div className="products-container">
+                            {user.favourites &&
+                                user.favourites.map((product) => (
+                                    <div className="product-card-container">
+                                        <Link
+                                            to={`/product/${product.id}`}
+                                            className="product-card-image"
+                                        >
+                                            <img src={product.imageLink} alt={product.name} />
+                                        </Link>
+                                        <div className="product-card-info">
+                                            <div className="product-card-info-left">
+                                                <div className="product-card-brand">Raymond</div>
+                                                <div className="product-card-name">
+                                                    {product.name}
+                                                </div>
+                                                <div className="product-card-price">
+                                                    Rs. {product.price}
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-                        ))}
-                </div>
+                                ))}
+                        </div>
+                    </>
+                ) : (
+                    <div className="user-empty-wishlist">
+                        <h1>Your wishlist is empty!!</h1>
+                    </div>
+                )}
             </div>
         </div>
     );
